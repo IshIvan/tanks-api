703ea029badd45e5232409357ca2376b7c0808d4
IshIvan <mngame51@gmail.com>
2018-01-29 00:19:19 +0300

    feat(bots): список классов для контроллеров

---
+++ src/bots/config.js
@@ -0,0 +1,8 @@
+import {ExampleBot} from "./example-bot";
+
+/**
+ * Словарь классов всех ботов, которые будут участвовать в игре.
+ */
+export const botClassesArray = [
+    ExampleBot
+];
---
+++ src/bots/example-bot.js
@@ -0,0 +1,8 @@
+import {Bot} from "../playground/bot";
+
+/**
+ * Тестовый бот.
+ */
+export class ExampleBot extends Bot {
+
+}
